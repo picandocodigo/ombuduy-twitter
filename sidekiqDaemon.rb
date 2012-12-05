f333d32d64318d23bc29ed1207240243ba3b9b72
Martin Loy <martin@guruhub.com.uy>
2012-12-04 22:00:41 -0200

    master sidekiq

---
+++ sidekiqDaemon.rb
@@ -0,0 +1,20 @@
+require 'sidekiq'
+require 'yaml'
+
+require './workers/new_tweet'
+
+config = YAML.load_file('config.yml')
+
+# Sidekiq server is multi-threaded so our Redis connection pool size defaults to concurrency (-c)
+Sidekiq.configure_server do |c|
+  c.redis = { :namespace => config['sidekiq']['namespace'], :url => config['sidekiq']['url'] }
+end
+
+# Start up sidekiq via
+# ./bin/sidekiq -r ./examples/por.rb
+#
+
+Sidekiq.configure_client do |c|
+  c.redis = { :namespace => config['sidekiq']['namespace'], :url => config['sidekiq']['url']}
+end
+
